a5bc6a5851f16f4e245175fb0da19f5f5bbbfb3b
AndreiSerbian <Serbian012@gmail.com>
2025-07-13 14:15:13 +0300

    modified:   js/components/contactsComponent.js 	modified:   js/components/homeComponent.js 	modified:   js/components/orderComponent.js 	modified:   js/components/productComponent.js

--- js/components/contactsComponent.js
+++ js/components/contactsComponent.js
@@ -31,7 +31,7 @@ const ContactsComponent = {
         <div class="container mx-auto px-6 py-3 flex justify-between items-center">
           <a href="#" class="text-xl font-bold text-gray-800">
          <img src="https://giftboxopt.ru/assets/logo-B0ADOiza.svg" alt="Logo" class="w-8 h-8" />
-          <span>SMT Premium Box</span>
+          <span class="inline">SMT Premium Box</span>
         </a>
           
           <!-- Навигационное меню (одинаковое для всех устройств) -->
